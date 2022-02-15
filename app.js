2f422f87e5083552878d6bf90bead6f03075b449
Ilona <ilona@mrchadd.nl>
2022-02-15 15:45:59 +0100

    Added logic to attack button

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = Vue.createApp({
             monsterHealth: 100,
         };
     },
+    computed: {
+        monsterBarStyles() {
+            return { width: this.monsterHealth + "%" };
+        },
+        playerBarStyles() {
+            return { width: this.playerHealth + "%" };
+        },
+    },
     methods: {
         getRandowmValue(min, max) {
             return Math.floor(Math.random() * (max - min)) + min;
