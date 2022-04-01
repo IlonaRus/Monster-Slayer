071e15c18792f73d4358d1a7a8dd1f95a9e44c2e
Ilona <ilona@mrchadd.nl>
2022-04-01 14:56:40 +0200

    Added start new game

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ const app = Vue.createApp({
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return { width: "0%" };
+            }
             return { width: this.monsterHealth + "%" };
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return { width: "0%" };
+            }
             return { width: this.playerHealth + "%" };
         },
         mayUseSpecialAttack() {
@@ -63,6 +69,12 @@ const app = Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
+        },
+        startNewGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.currentRound = 0;
         },
     },
 });
